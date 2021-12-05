479adecb2c6028a8840755f5ae0dc63013c3cda2
Felix Dorn <github@felixdorn.fr>
2021-12-05 15:33:55 +0000

    typo

--- src/Lexer.php
+++ src/Lexer.php
@@ -17,7 +17,6 @@ class Lexer
 {
     public static function lex(string $code): Stack
     {
-        // This token will be ignored as we directly call $stream->next() in our while loop in the parser.
         $tokens = new Stack();
 
         preg_match_all('/[,+\-*\/^%()]|\d*\.\d+|\d+\.\d*|\d+|[a-z_A-Z!]+[a-zA-Z0-9_]*|[ \t]+/', $code, $matches);
